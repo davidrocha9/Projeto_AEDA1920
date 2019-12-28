340fc19d04962c551c4760176f476a6ece1e2363
davidrocha9 <up201806371@fe.up.pt>
2019-12-28 10:36:47 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Selecao.h"
+#include "Menus.h"
+
+int main() {
+	Selecao selecao;   // create the national team
+
+	mainMenu(selecao); // initial menu inicial with the major options of the application
+
+	return 0;
+}
